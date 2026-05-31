a52932e0ae06efdd72120b1a97dd32b604c0028e
mistermooster11 <mistermooster11@gmail.com>
2026-05-31 09:42:00 +0700

    add sales popup (expires June 7 2026)

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import Footer from "@/components/custom/footer/Footer";
 import PageTransition from "@/components/custom/page-transition/PageTransition";
 import { Geist } from "next/font/google";
 import { cn } from "@/lib/utils";
+import SalePopup from "@/components/custom/popup/SalePopup"
 
 const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
@@ -24,6 +25,7 @@ export default function RootLayout({
     <html lang="en" className={cn("font-sans", geist.variable)}>
       <body>
         <PageTransition />
+        <SalePopup businessName="Bodnar Heating & Air Conditioning" expiryDate="June 7, 2026" trade="HVAC contractors" />
         <Header />
         <main>{children}</main>
         <Footer />
